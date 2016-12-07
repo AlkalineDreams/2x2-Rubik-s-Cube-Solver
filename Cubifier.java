3b68235bcf59a9adfbd91f4d657d885f28dfcd51
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-12-07 14:44:56 -0500

    Update Cubifier.java

--- Cubifier.java
+++ Cubifier.java
@@ -1,9 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Cubifier {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		FileReader fr = null;
+		String cubeFile = "rubikstest.txt"; //CHANGE THIS FILE TO CHANGE WHICH CUBE YOU'RE SOLVING
 		
+		BufferedReader br = null;
+		
+		fr = new FileReader(cubeFile);
+		br = new BufferedReader(fr);
+		
+		String line = br.readLine();
+		br.close();
+		
+		char[] inputArray = line.toCharArray();
+		
+		Cube cube = new Cube(inputArray);
+		
+		if (cube.Solve()){
+			
+			//if solution was found
+			System.out.println("A solution was found! Your cube's solution path is above.");
+			System.out.println("The solution path is printed with the next move above the last as you scroll up, with the solved cube at the top.");
+			
+		} else {
+			
+			//if no solution was found
+			System.out.println("Sorry, but we couldn't solve your cube.");
+			System.out.println("This was most likely an error on our end. Please send us a message about it!");
+			
+		}
 
 	}
 
