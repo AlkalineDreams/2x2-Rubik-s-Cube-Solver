1d6735f1c523ff8cea59e030f937b6d79e8e9a93
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-12-07 14:46:16 -0500

    Update Cube.java

--- Cube.java
+++ Cube.java
@@ -1,6 +1,25 @@
-
 public class Cube {
+
+	//properties
+	private Square[] _cube = new Square[24];
+	private Cube[] _tried;
 	
+	//constructor
+	public Cube(char[] inputArray){
+		
+		for (int counter = 0; counter < 24; counter++) {
+			
+			Square temp = new Square(inputArray[counter]);
+			this._cube[counter] = temp;
+			
+		}
+		
+	}
 	
+	public boolean Solve(){
+		
+		boolean solved = false;
+		
+	}
 
 }
