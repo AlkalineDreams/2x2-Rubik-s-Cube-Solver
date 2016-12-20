3c64338d6b4488896345730fe935b69eee59fd91
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-12-20 13:09:15 -0500

    Update Cube.java

--- Cube.java
+++ Cube.java
@@ -5,7 +5,8 @@ public class Cube {
 
 	//properties
 	private Square[] _cube = new Square[24];
-	private List<Cube> _tried = new ArrayList<Cube>();
+	//public static List<Square[]> _tried = new ArrayList<Square[]>();
+	TriedCubes listOfTries = new TriedCubes();
 	
 	//constructor
 	public Cube(char[] inputArray){
@@ -22,27 +23,28 @@ public class Cube {
 	
 	public boolean Solve(Cube cube){
 		
-		boolean solved = false;
-		boolean beenHereBefore = false;
+		cube.PrintCube(cube);
 		
-		for (int counter = 0; counter > this._tried.size(); counter++){
+		boolean solved = false;
+		
+		for (int counter = 0; counter < listOfTries.getLength(); counter++){
 			
-			if (cube == this._tried.get(counter)){
+			if (cube._cube == listOfTries.getList(counter)){
 				
 				//if this cube layout has been seen before, do nothing.
 				return false;
-				
-			} else {
-				
-				this._tried.add(cube);
 				
 			}
 			
 		}
 		
+		//now that we know this hasn't been tried yet, we're trying it
+		//TriedCubes._tried.add(cube._cube);
+		listOfTries.Add(cube._cube);
+		
 		if (CubeSolved(cube)){
 			
-			//PRINT OUT CUBE HERE
+			PrintCube(cube);
 			return true;
 			
 		} else {
@@ -69,7 +71,7 @@ public class Cube {
 		
 		if (solved){
 			
-			//PRINT OUT CUBE HERE
+			PrintCube(cube);
 			return true;
 			
 		} else {
@@ -77,6 +79,23 @@ public class Cube {
 			return false;
 			
 		}
+		
+	}
+	
+	public void PrintCube(Cube cube){
+		
+		//top
+		System.out.println("");
+		System.out.println("    " + cube._cube[16].getColor() + "" + cube._cube[17].getColor());
+		System.out.println("    " + cube._cube[18].getColor() + "" + cube._cube[19].getColor());
+		
+		//left, front, right, back
+		System.out.println(cube._cube[12].getColor() + "" + cube._cube[13].getColor() + " " + cube._cube[0].getColor() + "" + cube._cube[1].getColor() + " " + cube._cube[4].getColor() + "" + cube._cube[5].getColor() + " " + cube._cube[8].getColor() + "" + cube._cube[9].getColor());
+		System.out.println(cube._cube[14].getColor() + "" + cube._cube[15].getColor() + " " + cube._cube[2].getColor() + "" + cube._cube[3].getColor() + " " + cube._cube[6].getColor() + "" + cube._cube[7].getColor() + " " + cube._cube[10].getColor() + "" + cube._cube[11].getColor());
+		
+		//lower
+		System.out.println("    " + cube._cube[20].getColor() + "" + cube._cube[21].getColor());
+		System.out.println("    " + cube._cube[22].getColor() + "" + cube._cube[23].getColor());
 		
 	}
 	
@@ -163,27 +182,26 @@ public class Cube {
 		cube._cube[12] = temp;  //moves 14 to 12
 		
 		//back to top
-		temp = cube._cube[18];
-		temp2 = cube._cube[19];
-		cube._cube[18] = cube._cube[11];  //moves 11 to 18
-		cube._cube[19] = cube._cube[9];  //moves 9 to 19
+		temp = cube._cube[16];
+		temp2 = cube._cube[18];
+		cube._cube[16] = cube._cube[11];  
+		cube._cube[18] = cube._cube[9]; 
 		
 		//top to front
 		temp3 = cube._cube[0];
 		temp4 = cube._cube[2];
-		cube._cube[0] = temp;  //moves 18 to 0
-		cube._cube[2] = temp2; //moves 19 to 2
+		cube._cube[0] = temp;  
+		cube._cube[2] = temp2;
 		
 		//front to bottom
-		temp = cube._cube[21];
-		temp2 = cube._cube[20];
-		cube._cube[21] = temp3; //moves 0 to 21
-		cube._cube[20] = temp4; //moves 2 to 20
+		temp = cube._cube[20];
+		temp2 = cube._cube[22];
+		cube._cube[20] = temp3;
+		cube._cube[22] = temp4;
 		
 		//bottom to back
-		cube._cube[11] = temp;  //moves 21 to 11
-		cube._cube[9] = temp2; //moves 20 to 9
-		
+		cube._cube[11] = temp;
+		cube._cube[9] = temp2;
 		return cube;
 		
 	}
@@ -205,26 +223,26 @@ public class Cube {
 		cube._cube[16] = temp;  //moves 18 to 16
 		
 		//left to back
-		temp = cube._cube[10];
-		temp2 = cube._cube[11];
-		cube._cube[10] = cube._cube[15];  //moves 15 to 10
-		cube._cube[11] = cube._cube[13];  //moves 13 to 11
+		temp = cube._cube[9];
+		temp2 = cube._cube[8];
+		cube._cube[9] = cube._cube[13];
+		cube._cube[8] = cube._cube[12]; 
 		
 		//back to right
-		temp3 = cube._cube[4];
-		temp4 = cube._cube[6];
-		cube._cube[4] = temp;  //moves 10 to 4
-		cube._cube[6] = temp2; //moves 11 to 6
+		temp3 = cube._cube[5];
+		temp4 = cube._cube[4];
+		cube._cube[5] = temp;  
+		cube._cube[4] = temp2;
 		
 		//right to front
 		temp = cube._cube[1];
 		temp2 = cube._cube[0];
-		cube._cube[1] = temp3; //moves 4 to 1
-		cube._cube[0] = temp4; //moves 6 to 0
+		cube._cube[1] = temp3; 
+		cube._cube[0] = temp4; 
 		
-		//bottom to back
-		cube._cube[15] = temp;  //moves 1 to 15
-		cube._cube[13] = temp2; //moves 0 to 13
+		//front to left
+		cube._cube[13] = temp; 
+		cube._cube[12] = temp2;
 		
 		return cube;
 		
